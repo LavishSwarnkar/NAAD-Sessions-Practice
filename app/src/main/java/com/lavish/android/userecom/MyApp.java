1d0e8cf6727c25744fd62c576205e66e3c2c46df
Lavish Swarnkar <lavishswarnkar@gmail.com>
2020-11-24 20:13:55 +0530

    checkpoint 1

---
+++ app/src/main/java/com/lavish/android/userecom/MyApp.java
@@ -0,0 +1,14 @@
+package com.lavish.android.userecom;
+
+import android.app.Application;
+
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+
+}
